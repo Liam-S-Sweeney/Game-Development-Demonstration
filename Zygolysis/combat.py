2137ab3676e85f21fb275f6b6a006b0a25b9acad
Liam-S-Sweeney <liam.s.sweeney@outlook.com>
2025-12-29 12:16:45 -0500

    Update combat.py

--- Zygolysis/combat.py
+++ Zygolysis/combat.py
@@ -2,7 +2,6 @@ import character_table as ct
 import personality_stat_tracker as pst
 import stat_calc as sc
 import character_creator as cc
-# import items as items
 import tkinter as tk
 from tkinter import ttk, messagebox
 from background import *
@@ -145,7 +144,6 @@ def load_character_data():
         COMBAT_CONN.commit()
 
 load_character_data()
-# print(CHARACTER_DATA)
 
 def add_character(character):
 
@@ -162,12 +160,9 @@ def add_character(character):
         COMBAT_CURSOR.execute(insert_sql,row)
 
     COMBAT_CONN.commit()
-    # CHARACTERS_CONN.close()
-    # COMBAT_CONN.close()
 
 # ITEMS DATA FRAME
 items_df = pd.read_sql_query(f"SELECT * FROM items", ITEMS_CONN)
-# ITEMS_CONN.close()
 
 def delete_character(name):
     COMBAT_CURSOR.execute(f"DELETE FROM combat WHERE name = ?",(name,))
@@ -253,13 +248,6 @@ def relationships(character):
         relationships_dict[item] = cc.Character(character).__getattribute__(item)
     return relationships_dict
 
-# print(hp('test'))
-
-# print(hp('test'))
-# print(equipment('test'))
-
-# combat_fatigue('test')
-
 def equiped_gear(character):
     ITEMS_CURSOR.execute('SELECT * FROM items')
     column_names = [description[0] for description in ITEMS_CURSOR.description]
@@ -305,40 +293,32 @@ def bludgeon(attacker):
     all_wieldables = []
     for hand in range(4):
         hand_count = hand+1
-        # print(hand_count)
         equiped_in_hand = cc.Character(attacker).__getattribute__(f'hand_slot_{hand_count}')
         wielded_name = equiped_in_hand.split(' | ')[0]
         all_wieldables.append(wielded_name)
-        # print(all_wieldables)
-        # print(items_df)
     def hand_selection(): 
         hand_used = input("Which hand would you like to bludgeon with?\n\n 1 | 2 | 3 | 4\n\n")
         if int(hand_used) == 1:
             for i,item in enumerate(items_df['name']):
-                # print(item)
                 if all_wieldables[0] in item:
-                    # print(f'{item} is in row {i}')
                     bludgeoning_dmg = int(items_df.loc[i, "bludgeoning_dmg"])
                     total_dmg = bludgeoning_dmg + might_modifier
                     return total_dmg
         elif int(hand_used) == 2:
             for i,item in enumerate(items_df['name']):
                 if all_wieldables[1] in item:
-                    # print(f'{item} is in row {i}')
                     bludgeoning_dmg = int(items_df.loc[i, "bludgeoning_dmg"])
                     total_dmg = bludgeoning_dmg + might_modifier
                     return total_dmg
         elif int(hand_used) == 3:
             for i,item in enumerate(items_df['name']):
                 if all_wieldables[2] in item:
-                    # print(f'{item} is in row {i}')
                     bludgeoning_dmg = int(items_df.loc[i, "bludgeoning_dmg"])
                     total_dmg = bludgeoning_dmg + might_modifier
                     return total_dmg
         elif int(hand_used) == 4:
             for i,item in enumerate(items_df['name']):
                 if all_wieldables[3] in item:
-                    # print(f'{item} is in row {i}')
                     bludgeoning_dmg = int(items_df.loc[i, "bludgeoning_dmg"])
                     total_dmg = bludgeoning_dmg + might_modifier
                     return total_dmg
@@ -354,44 +334,34 @@ def pierce(attacker):
     all_wieldables = []
     for hand in range(4):
         hand_count = hand+1
-        # print(hand_count)
         equiped_in_hand = cc.Character(attacker).__getattribute__(f'hand_slot_{hand_count}')
         wielded_name = equiped_in_hand.split(' | ')[0]
         all_wieldables.append(wielded_name)
-        # print(all_wieldables)
-        # print(items_df)
     def hand_selection(): 
         hand_used = input("Which hand would you like to pierce with?\n\n 1 | 2 | 3 | 4\n\n")
         if int(hand_used) == 1:
             for i,item in enumerate(items_df['name']):
-                # print(item)
                 if all_wieldables[0] in item:
-                    # print(f'{item} is in row {i}')
                     piercing_dmg = int(items_df.loc[i, "piercing_dmg"])
                     total_dmg = piercing_dmg + might_modifier
                     return total_dmg
         elif int(hand_used) == 2:
             for i,item in enumerate(items_df['name']):
                 if all_wieldables[1] in item:
-                    # print(f'{item} is in row {i}')
                     piercing_dmg = int(items_df.loc[i, "piercing_dmg"])
                     total_dmg = piercing_dmg + might_modifier
                     return total_dmg
         elif int(hand_used) == 3:
             for i,item in enumerate(items_df['name']):
                 if all_wieldables[2] in item:
-                    # print(f'{item} is in row {i}')
                     piercing_dmg = int(items_df.loc[i, "piercing_dmg"])
                     total_dmg = piercing_dmg + might_modifier
                     return total_dmg
         elif int(hand_used) == 4:
             for i,item in enumerate(items_df['name']):
                 if all_wieldables[3] in item:
-                    # print(f'{item} is in row {i}')
                     piercing_dmg = int(items_df.loc[i, "piercing_dmg"])
                     total_dmg = piercing_dmg + might_modifier
-                    # print(f"{piercing_dmg} + {might_modifier}")
-                    # print(total_dmg)
                     return total_dmg
         else:
             print("None found")
@@ -404,70 +374,42 @@ def slash(attacker):
     all_wieldables = []
     for hand in range(4):
         hand_count = hand+1
-        # print(hand_count)
         equiped_in_hand = cc.Character(attacker).__getattribute__(f'hand_slot_{hand_count}')
         wielded_name = equiped_in_hand.split(' | ')[0]
         all_wieldables.append(wielded_name)
-        # print(all_wieldables)
-        # print(items_df)
 
     def hand_selection(): 
         hand_used = input("Which hand would you like to slash with?\n\n 1 | 2 | 3 | 4\n\n")
         if int(hand_used) == 1:
             for i,item in enumerate(items_df['name']):
-                # print(item)
                 if all_wieldables[0] in item:
-                    # print(f'{item} is in row {i}')
                     slashing_dmg = int(items_df.loc[i, "slashing_dmg"])
                     total_dmg = slashing_dmg + might_modifier
-                    # print(f'{total_dmg} dmg dealt')
                     return total_dmg
         elif int(hand_used) == 2:
             for i,item in enumerate(items_df['name']):
                 if all_wieldables[1] in item:
-                    # print(f'{item} is in row {i}')
                     slashing_dmg = int(items_df.loc[i, "slashing_dmg"])
                     total_dmg = slashing_dmg + might_modifier
-                    # print(f'{total_dmg} dmg dealt')
                     return total_dmg
         elif int(hand_used) == 3:
             for i,item in enumerate(items_df['name']):
                 if all_wieldables[2] in item:
-                    # print(f'{item} is in row {i}')
                     slashing_dmg = int(items_df.loc[i, "slashing_dmg"])
                     total_dmg = slashing_dmg + might_modifier
-                    # print(f'{total_dmg} dmg dealt')
                     return total_dmg
         elif int(hand_used) == 4:
             for i,item in enumerate(items_df['name']):
                 if all_wieldables[3] in item:
-                    # print(f'{item} is in row {i}')
                     slashing_dmg = int(items_df.loc[i, "slashing_dmg"])
                     total_dmg = slashing_dmg + might_modifier
-                    # print(f"{slashing_dmg} + {might_modifier}")
-                    # print(f'{total_dmg} dmg dealt')
-                    # print(type(total_dmg))
-                    # print(total_dmg)
                     return total_dmg
         else:
             print("None found")
             hand_selection()
     dmg = hand_selection()
-    # print(dmg)
-    # print(type(dmg))
     return dmg
     
-
-    
-# bludgeon('test')
-# print(pierce('test'))
-# answer = slash('test')
-# print(slash('test'))
-# print(answer)
-# test = slash('test')
-# print(test)
-# print(test)
-
 melee_strikes = {
     'punch': (punch,2),
     'kick': (kick,5),
@@ -476,6 +418,7 @@ melee_strikes = {
     'slash': (slash, 4),
 }
 
+
 def hit(attacker,strike,defender,limb):
     global CHARACTER_DATA
     global combat_characters_active_stats
@@ -483,15 +426,12 @@ def hit(attacker,strike,defender,limb):
     dmg_abosorbtion = cc.Character(defender).__getattribute__("dmg_absorbtion")[f'{sex(defender)}_{limb}']
 
     for key,val in combat_characters_active_stats.items():
-        # print(key,val)
         if defender in key:
             for target_limb,hp in val.items():
-                # print(target_limb,hp)
                 if limb in target_limb.lower():
                     limb_hp = hp
                     affected_limb = target_limb
                     affected_key = key
-                    # print(f"Limb is {limb}, limb hp is {limb_hp}")
     if strike.lower() in melee_strikes:
         if strike in ['punch','kick']:
             dmg = melee_strikes[strike.lower()]
@@ -536,8 +476,6 @@ def hit(attacker,strike,defender,limb):
             combat_characters_active_stats[affected_key][affected_limb] = new_limb_hp
             # additional debuffs depending on percentage of dmg here
 
-# hit('op','slash','weak','quads_r')
-
 def static_melee_aim(attacker,strike,defender,limb):
     global combat_characters_active_stats
     attacker = cc.Character(attacker).__getattribute__('name')
@@ -546,7 +484,6 @@ def static_melee_aim(attacker,strike,defender,limb):
     for key in combat_characters_active_stats.keys():
         if attacker in key:
             attacker_key = key
-            # print(attacker_key)
         if defender in key:
             defender_key = key
     
@@ -558,7 +495,6 @@ def static_melee_aim(attacker,strike,defender,limb):
     if can_move == True:
         if melee_accuracy_val * (100/cc.Character(attacker).__getattribute__('aim')[f'{sex(attacker)}_hand_eye_coord']) > percent_roll:
             percent_roll = random.randint(0,100)
-            # if combat_characters_active_stats[attacker_key]
             if (cc.Character(defender).__getattribute__('parry')[f'{sex(defender)}']* defender_fatigue_modifier) > percent_roll:
                 print(f'⚔️ PARRY⚔️')
             else:
@@ -583,26 +519,17 @@ def combat(*args):
 
     [[add_character(character)] for character in args]
     load_character_data()
-
-    # print(CHARACTER_DATA)
+    
     for num,character in enumerate(args):
         key = f'{num}_{character}'
         if key not in combat_characters_active_stats:
             combat_characters_active_stats[key] = {}
             for part,hp_val in hp(character).items():
                 combat_characters_active_stats[key][part] = hp_val
-            # combat_characters_active_stats[key] = inventory(character)
             for etype,armor_name in equipment(character).items():
                 combat_characters_active_stats[key][etype] = armor_name
-            # combat_characters_active_stats[key] = hand_slots(character)
-            # combat_characters_active_stats[key] = accessories(character)
-            # combat_characters_active_stats[key] = status_effects(character)
-            # combat_characters_active_stats[key] = stress(character)
-            # combat_characters_active_stats[key] = relationships(character)
             combat_characters_active_stats[key]['total_fatigue'] = 0
             combat_characters_active_stats[key]['fatigue_level'] = ("no",1)
-
-    # print(combat_characters_active_stats)
 
     combat_continues = True
     while combat_continues == True:
@@ -651,7 +578,6 @@ def combat(*args):
                                 
                     list_of_limbs = []
                     [[list_of_limbs.append((limb.split(f"{sex(defender)}_"))[1])] for limb in hp(defender).keys()]
-                    # [[list_of_limbs.append(limb)] for limb in hp(defender).keys()]
                     print(list_of_limbs)
 
                     limb = input(f"\nWhat limb would you like to target?\n").lower()
@@ -665,8 +591,6 @@ def combat(*args):
                             else:
                                 try_again = True
 
-
-                    # print(attacker)
                     static_melee_aim(attacker,strike,defender,limb)
                     if strike.lower() in melee_strikes:
                         added_fatigue = melee_strikes[strike.lower()][1]
@@ -706,11 +630,4 @@ def combat(*args):
     [[delete_character(character)] for character in args]
 
 combat("op",'weak')
-
-# print(static_melee_aim())
-
-### ADD Damge Over Time, ENERGY/FATIGUE, ARMOR + EQUIPMENT, RANGED, and ACTIVE
-
-# MAKE IT SO THAT DIFFERENT ATTACKS TAKE UP A DIFFERENT NUMBER OF FATIGUE / MOVES
-# ENABLE AUTOREST TO CERTAIN FATIGUE THRESHOLDS
 
